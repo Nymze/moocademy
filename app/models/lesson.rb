08e1ea344aefe69c5859d4792e99f3831660f310
Arnaud <arnaud.meneroud@gmail.com>
2018-02-01 18:05:03 +0000

    second push

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,12 +1,13 @@
 class Lesson < ApplicationRecord
-	 belongs_to :course
+	 belongs_to :course # la leçon appartient à un cours 
 
-  validates :titre, presence: true,
-                    length: { maximum: 255 },
-                    unique: true
+  validates :titre, presence: true, # un titre est exigé 
+                    length: { maximum: 140}, # pas plus de 140 characteres
+                    uniqueness: {:message => "Title already exists."} #le titre doit etre unique
 
-  validates :description, presence: true
 
-  validates :course_id, presence: true,
-                        only_integer: true
+  validates :description, presence: true # description exigée
+
+  validates :course_id, presence: true, # l'id du cours est requis
+                        only_integer: true # et ne prends que les integers
 end
